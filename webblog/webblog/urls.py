04a22ce6d70d83f0bbd520e1c905bcd0e5289896
SuperNova <nilu.shahmoradi@gmail.com>
2024-09-18 17:28:20 +0200

    styles

--- webblog/webblog/urls.py
+++ webblog/webblog/urls.py
@@ -4,11 +4,12 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
+from articles.views import articles_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about),
-    path('', views.home),
+    path('', articles_list),
     path('articles/', include('articles.urls')),
     path('accounts/', include('accounts.urls')),
 ]
